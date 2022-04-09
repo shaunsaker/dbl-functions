c735b7c9a7df0c1dda36601ea370aacc930660bf
Shaun Saker <shaun.saker@timeular.com>
2022-04-09 13:26:26 +0200

    fix/resolve deleteBlockchainHDWallet

--- src/services/blockCypher/deleteBlockchainHDWallet.ts
+++ src/services/blockCypher/deleteBlockchainHDWallet.ts
@@ -7,6 +7,8 @@ export const deleteBlockchainHDWallet = async (name: string): Promise<void> => {
       await axios.delete(
         `${process.env.BLOCK_CYPHER_API}/wallets/hd/${name}?token=${process.env.BLOCK_CYPHER_TOKEN}`,
       );
+
+      resolve();
     } catch (error: Error | AxiosError | unknown) {
       if (axios.isAxiosError(error) && error.response) {
         reject(new Error(JSON.stringify(error.response.data)));
