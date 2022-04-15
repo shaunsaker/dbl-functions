ece0a8b76442fb568704e187886a699191cfac80
Shaun Saker <shaun.saker@timeular.com>
2022-04-15 10:49:41 +0200

    feature/remove unneeded blockCypher

--- src/services/blockCypher/getBlockchainHDWalletAddresses.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const getBlockchainHDWalletAddresses = async (name: string) => {
-  return (
-    await axios.get(
-      `${process.env.BLOCK_CYPHER_API}/wallets/hd/${name}/addresses?token=${process.env.BLOCK_CYPHER_TOKEN}`,
-    )
-  ).data;
-};
--- src/services/blockCypher/listBlockchainAddressWebhooks.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const listBlockchainAddressWebhooks = async (): Promise<{
-  id: string;
-}> => {
-  return await axios.get(
-    `${process.env.BLOCK_CYPHER_API}/hooks?token=${process.env.BLOCK_CYPHER_TOKEN}`,
-  );
-};
