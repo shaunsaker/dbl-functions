0dab4023951e22de00b57bc17fb6215c0ba60e81
Shaun Saker <sakershaun@gmail.com>
2022-04-19 12:49:15 +0200

    fix/pull payment end point

--- src/services/btcPayServer/createPullPayment.ts
+++ src/services/btcPayServer/createPullPayment.ts
@@ -15,7 +15,7 @@ export const createPullPayment = async (
 ): Promise<Response> => {
   return new Promise(async (resolve, reject) => {
     try {
-      const endpoint = `${BtcPayServerEndpoint.stores}/${storeId}/payment-methods/OnChain/BTC/wallet/transactions`;
+      const endpoint = `${BtcPayServerEndpoint.stores}/${storeId}/pull-payments`;
 
       const data = await btcPayServerApi.post<Response>(endpoint, payload);
 
