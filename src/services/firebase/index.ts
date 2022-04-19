d04940cffad450c103d4e27cd51003dfc348f293
Shaun Saker <sakershaun@gmail.com>
2022-04-19 10:32:30 +0200

    fix/remove half-baked firebase logging

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,8 +1,5 @@
 import * as firebase from 'firebase-admin';
 
-// adds compatibility for console.log
-require('firebase-functions/lib/logger/compat');
-
 const serviceAccount = require('./service-account.json');
 
 firebase.initializeApp({
